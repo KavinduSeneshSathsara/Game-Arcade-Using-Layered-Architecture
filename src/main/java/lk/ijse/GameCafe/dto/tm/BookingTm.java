06f0830d4076c4f7056276b1ebd41b5aadda5f9e
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2024-01-01 13:31:22 +0530

    bug fixed

---
+++ src/main/java/lk/ijse/GameCafe/dto/tm/BookingTm.java
@@ -0,0 +1,22 @@
+package lk.ijse.GameCafe.dto.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookingTm {
+    private String bookingId;
+    private String cus_id;
+    private Date bookingDate;
+    private Time bookingTime;
+    private Time startTime;
+    private Time endTime;
+    private String status;
+    private Double total;
+}
