6aa18ded7e028f482e6cf22aa99d1902efbe3833
Kavindu Senesh <140303230+KavinduSeneshSathsara@users.noreply.github.com>
2024-01-12 00:57:41 +0530

    fully refracted

---
+++ src/main/java/lk/ijse/GameCafe/entity/Custom.java
@@ -0,0 +1,15 @@
+package lk.ijse.GameCafe.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Custom {
+    private String bookingId;
+    private String cus_id;
+    private Date bookingDate;
+    private Time bookingTime;
+    private Time startTime;
+    private Time endTime;
+    private String status;
+    private Double total;
+}
